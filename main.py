6f76778870158d08cedf5cef7497c33bdb6a8ac6
dotan <frenkeldotan1998@gmail.com>
2023-04-13 14:14:17 +0300

    Add invoice date

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import glob   # we use this library when we hava multiple file, and we save those path inside a list
 from fpdf import FPDF
 from pathlib import Path
+from datetime import datetime
 
 
 
@@ -12,10 +13,19 @@ for path in filepaths:
     df = pd.read_excel(path, sheet_name="Sheet 1")   # when we use multiple files we need to define the key sheet file
     pdf = FPDF(orientation='P', unit='mm', format='A4')
     pdf.add_page()
+
     filename = Path(path).stem
     invoice_nr = filename.split('-')[0]
+    format_date = datetime.now().strftime('%d-%m-%y')
+
     pdf.set_font(family="Arial", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice number.{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice number.{invoice_nr}", ln=1)
+
+    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {format_date}")
+
+
+
     pdf.output(f'PDFs/{filename}.pdf')
 
 
