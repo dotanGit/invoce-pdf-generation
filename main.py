6be307a041c9215f1a66ff4629c47be0b658a8f4
dotan <frenkeldotan1998@gmail.com>
2023-04-13 13:59:37 +0300

    Create a minimal PDF for each Excel file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import glob   # we use this library when we hava multiple file, and we save those path inside a list
+from fpdf import FPDF
+from pathlib import Path
+
 
 
 filepaths = glob.glob('invoices/*.xlsx')   # *.xlsx mean that we import every file in the folder that is xlsx type
@@ -7,4 +10,12 @@ print(filepaths)
 
 for path in filepaths:
     df = pd.read_excel(path, sheet_name="Sheet 1")   # when we use multiple files we need to define the key sheet file
-    print(df)
+    pdf = FPDF(orientation='P', unit='mm', format='A4')
+    pdf.add_page()
+    filename = Path(path).stem
+    invoice_nr = filename.split('-')[0]
+    pdf.set_font(family="Arial", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice number.{invoice_nr}")
+    pdf.output(f'PDFs/{filename}.pdf')
+
+
